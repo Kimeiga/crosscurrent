803789102c304082b0df19862dbc270a4db9adf0
Hakan Alpay <hak7alp@gmail.com>
2026-09-23 20:05:58 -0400

    Install presentation-only GPU battlefields

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './styles.css';
 import { mount } from 'svelte';
 import App from './App.svelte';
+import { installBattlefields } from './battlefields';
 
 mount(App, { target: document.getElementById('app')! });
+installBattlefields(document.getElementById('app')!);
